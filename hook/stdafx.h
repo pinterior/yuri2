56cc6a0ab5f5282212ed6d30b91ff9de8ab01f80
pinterior <pinterior@users.noreply.github.com>
2016-03-06 01:49:48 +0900

    trivial

--- hook/stdafx.h
+++ hook/stdafx.h
@@ -3,6 +3,7 @@
 #include "../common/targetver.h"
 
 #define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
 #include <windows.h>
 
 #include "dllmain.h"
--- main/stdafx.h
+++ main/stdafx.h
@@ -8,6 +8,7 @@
 #include <utility>
 
 #define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
 #include <windows.h>
 
 #include <atlbase.h>
