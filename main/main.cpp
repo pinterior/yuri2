6ea108770887a4534b81417e76cba58011ac1fac
pinterior <pinterior@users.noreply.github.com>
2016-03-06 01:47:04 +0900

    update dialog appearance

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 CAppModule appModule;
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
